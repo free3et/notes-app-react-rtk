061c0dc9f1d29df0d1916aaf13b00da12414fad5
free3et <free3et@gmail.com>
2023-08-02 17:33:16 +0300

    fix path vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/note-app-react-rtk/",
+  base: "/notes-app-react-rtk/",
 });
